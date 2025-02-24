a235676b0c3ec2192485b20c69d932958028c461
Cullen Gray <cgray@c3r4s2.42prague.com>
2025-02-24 16:59:08 +0100

    formatting

--- 0_Starting/ex00/Hello.py
+++ 0_Starting/ex00/Hello.py
@@ -7,12 +7,15 @@ ft_dict		= {"Hello" : "titi!"}
 
 ft_list[1] = "World!"
 
-ft_tuple = ft_tuple[:1] + ("Czechia!", ) + ft_tuple[2:] #tuples are immutable, concat to new tuple
+#tuples are immutable, concat to new tuple
+ft_tuple = ft_tuple[:1] + ("Czechia!", ) + ft_tuple[2:] 
 
+#sets are unordered and unchangable, have to remove and add new value
 ft_set.remove("tutu!")
 ft_set.add("Prague!")
 
-ft_dict.update({"Hello": "42Prague!"}) #replace key Hello with correct value
+#replace key Hello with correct value
+ft_dict.update({"Hello": "42Prague!"})
 
 
 print(ft_list)
