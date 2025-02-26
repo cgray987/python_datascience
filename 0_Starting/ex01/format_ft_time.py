d59e20e2bd3300ab04396d4c536e25747bf2500a
Cullen Gray <cgray@c3r3s1.42prague.com>
2025-02-26 15:09:14 +0100

    rebase

--- 0_Starting/ex01/format_ft_time.py
+++ 0_Starting/ex01/format_ft_time.py
@@ -1,16 +1,15 @@
 import time
 
 # Epoch is the time from which time.time() measures.
-obj = time.gmtime(0)
-epoch = time.strftime("%B %-d, %Y", obj)
-epoch += ":"
+# obj = time.gmtime(0)
+# epoch = time.asctime(obj)
 
 t = time.time()
-t_comma = '{:,.4f}'.format(t)
+t_comma = "{:,.4f}".format(t)
 
-t_sci = '{:.2e}'.format(t)
-current_date = time.strftime("%B %d %Y")
+t_sci = "{:.2e}".format(t)
+current_date = time.strftime("%B %d, %Y")
 
-print("Seconds since", epoch, t_comma, 
-	  "or", t_sci, "in scientific notation")
+print(f"Seconds since January 1, 1970: {t_comma} or {t_sci} "
+      f"in scientific notation")
 print(current_date)
--- 0_Starting/ex05/building.py
+++ 0_Starting/ex05/building.py
@@ -1,26 +1,36 @@
 import sys
 
-# def word_count(s: str) -> int:
-	# characters = len(str)
 
-	# for c in 
+def word_count(s: str) -> int:
+    """returns character counts for given string"""
 
-	# upper = sum(1 for c in s if c.isupper())
-	# lower = sum(1 for c in s if c.islower())
-	# punc = sum(1 for c in s if c in string.punctuation)
-	# spaces = sum(1 for )
-	
-	
+    characters = len(s)
+    punctuation = "!”#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+    upper = sum(1 for c in s if c.isupper())
+    lower = sum(1 for c in s if c.islower())
+    punc = sum(1 for c in s if c in punctuation)
+    spaces = sum(1 for c in s if c.isspace())
+    digits = sum(1 for c in s if c.isdigit())
+
+    print(f"The text contains {characters} characters:")
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{punc} punctuation marks")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
+
 
 def main():
-	ac = len(sys.argv)
+    ac = len(sys.argv)
 
-	assert ac < 1, "Too many arguments for this puny program"
-	if (ac == 1):
-		
-	else:
-		
+    assert ac <= 2, "Too many arguments for this puny program"
+    if ac == 1:
+        s = input("Give me a string: ")
+    else:
+        s = sys.argv[1]
+    word_count(s)
 
 
 if __name__ == "__main__":
-	main()
+    main()
