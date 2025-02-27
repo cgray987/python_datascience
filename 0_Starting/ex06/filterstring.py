9040f37f748f3929688f8b54dd66e95a9a3c0db7
Cullen Gray <cgray@c3r3s3.42prague.com>
2025-02-27 14:40:38 +0100

    finish ex06, create tests

--- 0_Starting/ex06/filterstring.py
+++ 0_Starting/ex06/filterstring.py
@@ -1,30 +1,34 @@
 import sys
-import ft_filter
+from ft_filter import ft_filter
 
 
 def main():
-    try:
-        ac = len(sys.argv)
-        if (ac != 3):
-            raise AssertionError("the arguments are bad")
-        S = sys.argv[1]
-        N = int(sys.argv[2])
-        if N is None:
-            raise AssertionError("the arguments are bad")
-        if S.find("!”#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"):
-            raise AssertionError("the arguments are bad")
-		if not isinstance(S, N) or not isinstance(N, int):
+	""" Takes two args, String S and int N
+	 
+	Splits string into words by spaces and filters
+	those that are shorter than N """
+	try:
+		ac = len(sys.argv)
+		if (ac != 3):
 			raise AssertionError("the arguments are bad")
-        
-		tab = S.split(" ")
-        filtered = ft_filter(lambda word: len(word) > N, tab)
-        for x in filtered:
-            print(x)
-        
+		input_str = sys.argv[1]
+		try:
+			min_word_len = int(sys.argv[2])
+		except ValueError:
+			raise AssertionError("the arguments are bad")
+		if not isinstance(input_str, str) or not isinstance(min_word_len, int):
+			raise AssertionError("the arguments are bad")
+		if input_str.find("!”#$%&'()*+,-./:;<=>?@[\\]^_`{|}~") != -1:
+			raise AssertionError("the arguments are bad")
+		
+		L = lambda word: len(word) > min_word_len
+		filtered = list(ft_filter(L, input_str.split(" ")))
+		print(filtered)
+		
 
-    except AssertionError as e:
-        print(f"AssertionError: {str(e)}")
+	except AssertionError or ValueError as e:
+		print(f"AssertionError: {str(e)}")
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- 0_Starting/ex06/tester.py
+++ 0_Starting/ex06/tester.py
@@ -1,7 +1,45 @@
+from ft_filter import ft_filter
 
+
+class c:
+	RED = "\033[0;31m"
+	GRN = "\033[0;32m"
+	CYN = "\033[0;36m"
+	RST = "\033[0m"
+
+
+def print_ok_ko(test, mine, expected):
+	""" prints test results """
+	print(f"{c.CYN}~~~~Test: {test}~~~~{c.RST}")
+	print(mine)
+	print(expected)
+	if mine == expected:
+		print(c.GRN + "~~~~\tOK\t~~~~" + c.RST)
+	else:
+		print(c.RED + "~~~~\tKO\t~~~~" + c.RST)
+		
 
 def main():
-	
+	""" tests ft_filter against filter """
+	list1 = [1, 2, 3, 4, 5, 6]
+	lambda1 = lambda x: x % 2 ==0
+	test1 = list(ft_filter(lambda1, list1))
+	expected1 = list(filter(lambda1, list1))
+	print_ok_ko("even number list", test1, expected1)
+
+	list2 = ["array of strings", 1, 42.42, "YELLING", "somewHAT YELLIng", "dig1235", "DIG12345"]
+	def lower(string):
+		if (isinstance(string, str)):
+			return string.islower()
+	test2 = list(ft_filter(lower, list2))
+	expected2 = list(filter(lower, list2))
+	print_ok_ko("lower filter", test2, expected2)
+
+	list3 = ["mix types", 1, 0, None, "1", 42.42, [1, 2], "test", 999999999999999]
+	lambda3 = lambda x: isinstance(x, int)
+	test3 = list(ft_filter(lambda3, list3))
+	expected3 = list(filter(lambda3, list3))
+	print_ok_ko("mixed types", test3, expected3)
 
 if __name__ == "__main__":
 	main()
