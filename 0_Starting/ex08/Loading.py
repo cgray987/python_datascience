f7efc49dc7248912095ddae443f2f411d8badd88
Cullen Gray <cgray@c3r3s3.42prague.com>
2025-02-27 16:54:16 +0100

    progress bar! ex08

---
+++ 0_Starting/ex08/Loading.py
@@ -0,0 +1,44 @@
+import time # for clock
+import shutil # to get terminal size (width)
+
+def format_time(time) -> str:
+	""" takes time in seconds, returns formatted string MM:SS """
+	min, sec = divmod(time, 60)
+	return f"{int(min):02d}:{int(sec):02d}"
+
+def ft_tqdm(lst: range) -> None: # type: ignore
+	"""A function to generate a progress bar based on the given
+	iterable (lst). Displays various percentage, bar, total size and timings
+
+	Does not work well when terminal is smaller than 15 columns
+	(but it doesn't crash ;)"""
+
+	print_info = True
+	total_items = len(lst)
+	info_len = (len(str(total_items)) * 2) + 5
+	start_time = time.time()
+	terminal_buffer = 30
+	terminal_width = shutil.get_terminal_size().columns - 30
+	progress_bar_width = terminal_width - info_len
+	if (terminal_width <= 15):
+		print_info = False
+		terminal_width = 1
+		progress_bar_width = terminal_width
+
+	for i, item in enumerate(lst, 1):
+		iteration_size = int(i / total_items * progress_bar_width)
+		time_since = time.time() - start_time
+		speed = i / time_since
+		finish_time = (total_items - i) / speed
+		progress_percent = i * 100 // total_items
+
+		if (i % 10 == 0 or i == total_items):
+			bar = '█' * iteration_size
+			progress_bar = f"|{bar:<{progress_bar_width}}|"
+			time_formated = f"{format_time(time_since)}<{format_time(finish_time)},"
+			full_line = f"{progress_percent:3}%{progress_bar}"
+			info = f" {i}/{total_items} [{time_formated} {speed:.2f}it/s]"
+			if print_info:
+				full_line += info
+			print(f"\r{full_line:<{terminal_width - 1}}", end="", flush=True)
+		yield item
